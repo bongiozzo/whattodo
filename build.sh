009d6ee1cf9dfc3f8cae02cc502d77ea134022a5
Serge Polyakov <sergey.polyakov@hotmail.com>
2024-11-20 07:09:49 +0300

    antora tuning

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 rm -fr ./build
-
-cp source/* formats/antora/modules/ROOT/pages/
 npx antora antora-playbook.yml
-rm formats/antora/modules/ROOT/pages/*
 touch build/site/.nojekyll
 
-asciidoctor-epub3 -a epub3-stylesdir=formats/epub -r asciidoctor-mathematical -D build/site wtd.adoc
-asciidoctor-pdf --theme formats/pdf/wtd.yml -r asciidoctor-mathematical -D build/site wtd.adoc
+asciidoctor-epub3 -a epub3-stylesdir=../../../epub -r asciidoctor-mathematical -D build/site formats/antora/modules/ROOT/wtd.adoc
+asciidoctor-pdf --theme formats/pdf/wtd.yml -r asciidoctor-mathematical -D build/site formats/antora/modules/ROOT/wtd.adoc
 asciidoctor-pdf-optimize build/site/wtd.pdf
