4ec13bba68fa02c4b03f9a897ab40dc309487d7b
Serge Polyakov <sergey.polyakov@hotmail.com>
2024-11-20 09:57:53 +0300

    ru lang prefix and folder structure refinement

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ rm -fr ./build
 npx antora antora-playbook.yml
 touch build/site/.nojekyll
 
-asciidoctor-epub3 -a epub3-stylesdir=../../../epub -r asciidoctor-mathematical -D build/site formats/antora/modules/ROOT/wtd.adoc
-asciidoctor-pdf --theme formats/pdf/wtd.yml -r asciidoctor-mathematical -D build/site formats/antora/modules/ROOT/wtd.adoc
-asciidoctor-pdf-optimize build/site/wtd.pdf
+asciidoctor-epub3 -a epub3-stylesdir=epub -r asciidoctor-mathematical -D build/site/ru wtd.adoc
+asciidoctor-pdf --theme pdf/wtd.yml -r asciidoctor-mathematical -D build/site/ru wtd.adoc
+asciidoctor-pdf-optimize build/site/ru/wtd.pdf
