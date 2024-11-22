e810b76b36cead7fe4fa6cf428009636c90521ca
Serge Polyakov <sergey.polyakov@hotmail.com>
2024-11-22 10:46:23 +0300

    massive antora changes

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+export CI=true
+
 rm -fr ./build
 npx antora antora-playbook.yml
 touch build/site/.nojekyll
 
-asciidoctor-epub3 -a epub3-stylesdir=epub -r asciidoctor-mathematical -D build/site/ru wtd.adoc
-asciidoctor-pdf --theme pdf/wtd.yml -r asciidoctor-mathematical -D build/site/ru wtd.adoc
+asciidoctor-epub3 -a epub3-stylesdir=epub -D build/site/ru wtd.adoc
+asciidoctor-pdf --theme pdf/wtd.yml -D build/site/ru wtd.adoc
 asciidoctor-pdf-optimize build/site/ru/wtd.pdf
