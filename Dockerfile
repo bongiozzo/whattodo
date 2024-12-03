e410e3c8891f34087e6335dbaee3f263722a1660
Serge Polyakov <beer-bong@yandex.ru>
2024-12-03 17:19:46 +0300

    Added Lunr Search functionality

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM antora/antora:testing
+
+RUN npm i @antora/lunr-extension
--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+# docker run -u $(id -u) -v $PWD:/antora:Z --rm -t whattodo:latest antora-playbook.yml
 export CI=true
-
 rm -fr ./public
 npx antora antora-playbook.yml
 
+# docker ???
 asciidoctor-epub3 -a epub3-stylesdir=epub -D public/ru wtd.adoc
 asciidoctor-pdf --theme pdf/wtd.yml -D public/ru wtd.adoc
 asciidoctor-pdf-optimize public/ru/wtd.pdf
