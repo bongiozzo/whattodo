2d5d72e0e5432c56d1b2c1af9a5a8589caad3ab8
Serge Polyakov <beer-bong@yandex.ru>
2025-08-31 21:10:59 +0400

    Massive Grammar fixes, JPG optimizations, DeepWiki AI-companion, book.txt for AsciiDoc rename.

---
+++ resize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+find . -type f \( -iname "*.jpg" -o -iname  "*.png" \) -exec identify -format "%B %f: %wx%h\n" {} + | sort -h
+for img in *.jpg; do
+  # Get image width and height
+  read width height < <(magick identify -format "%w %h" "$img")
+  
+  # Check if width or height is greater than 100
+  if [ "$width" -gt 1920 ] || [ "$height" -gt 1080 ]; then
+    magick mogrify -resize '1920x1080>' "$img"
+  fi
+done
+find . -type f \( -iname "*.jpg" -o -iname  "*.png" \) -exec identify -format "%B %f: %wx%h\n" {} + | sort -h
