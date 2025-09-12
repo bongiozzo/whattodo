2bc46b46eaf27f559d208888cfa37e54ebf2ceb8
Serge Polyakov <sergey.polyakov@hotmail.com>
2025-09-12 20:49:24 +0400

    Family Bakery photo - images optimization

--- scripts/resize-images.sh
+++ scripts/resize-images.sh
@@ -3,14 +3,17 @@
 root="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$root"
 
-find . -type f \( -iname "*.jpg" -o -iname  "*.png" \) -exec identify -format "%B %f: %wx%h\n" {} + | sort -h
-for img in *.jpg; do
+images_dir="ru/modules/ROOT/images"
+
+images=$(find "$images_dir" -type f \( -iname "*.jpg" -o -iname "*.png" \))
+for img in $images; do
   # Get image width and height
   read width height < <(magick identify -format "%w %h" "$img")
   
   # Check if width or height is greater than 100
   if [ "$width" -gt 1920 ] || [ "$height" -gt 1080 ]; then
     magick mogrify -resize '1920x1080>' "$img"
+    echo "Resized $img from ${width}x${height}"
   fi
 done
-find . -type f \( -iname "*.jpg" -o -iname  "*.png" \) -exec identify -format "%B %f: %wx%h\n" {} + | sort -h
+find "$images_dir" -type f \( -iname "*.jpg" -o -iname  "*.png" \) -exec identify -format "%B %f: %wx%h\n" {} + | sort -h
